43250cf2ecd1f3d04485d73dadc85894f6b950bc
Claudia Ramirez <mely.ramiz07@gmail.com>
2022-07-13 23:30:49 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from '../components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navbar />
+      </header>
+      <body>
+        
+      </body>
+    </div>
+
+  );
+}
+
+export default App;
